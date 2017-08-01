95dff4ad19f9f915f01a3a563b40f71ecf108566
Eric <dengyangself@163.com>
2017-08-01 09:57:36 +0800

    add related object Choice to Question in polls

--- polls/admin.py
+++ polls/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInLine]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+
